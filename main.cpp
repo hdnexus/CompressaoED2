8326348829c0b8f00c398f5ca6b71d4d82f03950
Gabriel <dotaes22@gmail.com>
2021-03-17 02:11:16 -0300

    Linux

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-
 /* Comentários
 1- Professor, antes de tudo, gostaria de falar que utilizei o 0 como primeiro output Index da tabela, vi alguns exemplos que começavam com 1, 
 outros com 0, só que não achei nada falando qual era o modelo correto.
@@ -22,98 +21,100 @@ Ex:  97 a
 void createInitialTable(string sTotal, vector<string> &dictionary)
 {
   //Peguei cada caractere da string, transformei eles em string e dei push no vector dictionary
-  for(int i=0; i < sTotal.size(); i++)
+  for (int i = 0; i < sTotal.size(); i++)
   {
     char c = sTotal[i];
-    string sTotal(1,c);
+    string sTotal(1, c);
     dictionary.push_back(sTotal);
   }
 }
 
-void compression(string sTotal, vector<int> &codified, vector<string> &dictionary){
+void compression(string sTotal, vector<int> &codified, vector<string> &dictionary)
+{
   int index;
   char cx = sTotal[0];
-  string P(1,cx);
+  string P(1, cx);
   string C;
-  for(int i=1; i < sTotal.size(); i++){
-      C = sTotal[i];
-      vector<string>::iterator itr = find(dictionary.begin(), dictionary.end(), P + C);
-      if (itr != dictionary.end())
-      {
-        P = P + C;
-      }
-      else
-      {
-        vector<string>::iterator itrP = find(dictionary.begin(), dictionary.end(), P);
-        index = distance(dictionary.begin(), itrP);
-        codified.push_back(index);
-        dictionary.push_back(P + C);
-        P = C;
-      }
+  for (int i = 1; i < sTotal.size(); i++)
+  {
+    C = sTotal[i];
+    vector<string>::iterator itr = find(dictionary.begin(), dictionary.end(), P + C);
+    if (itr != dictionary.end())
+    {
+      P = P + C;
+    }
+    else
+    {
+      vector<string>::iterator itrP = find(dictionary.begin(), dictionary.end(), P);
+      index = distance(dictionary.begin(), itrP);
+      codified.push_back(index);
+      dictionary.push_back(P + C);
+      P = C;
+    }
   }
   vector<string>::iterator itrP = find(dictionary.begin(), dictionary.end(), P);
   index = distance(dictionary.begin(), itrP);
   codified.push_back(index);
-
 }
 
 string decompression(vector<string> &dictionary, vector<int> &codified)
 {
   string auxString;
   string finalString;
-  for(int x=0; x < codified.size(); x++)
+  for (int x = 0; x < codified.size(); x++)
   {
-      int indexDecodify = codified[x];
-      auxString = dictionary[indexDecodify];
-      finalString = finalString + auxString;
+    int indexDecodify = codified[x];
+    auxString = dictionary[indexDecodify];
+    finalString = finalString + auxString;
   }
   return finalString;
 }
 
 void printCodified(vector<int> &codified, ofstream &arq)
 {
-  arq << "\nCodificacao" << endl;
-  for(int n=0; n < codified.size(); n++)
+  cout << "\nCodificacao" << endl;
+  for (int n = 0; n < codified.size(); n++)
   {
-      arq << codified[n] << "   " ;
+    cout << codified[n] << "   ";
   }
 }
 
 void printDecodify(string finalString, ofstream &arq)
 {
-  arq << "\n\nDecodificacao\n" << finalString;
+  cout << "\n\nDecodificacao\n"
+       << finalString << endl;
 }
 
 void printInitialTable(vector<string> &dictionary, ofstream &arq)
 {
   //Imprimindo a tabela de simbolos inicial
-  arq << "\nTabela de Simbolos Inicial" << endl;
-  for(int p=0; p < dictionary.size();p++)
+  cout << "\nTabela de Simbolos Inicial" << endl;
+  for (int p = 0; p < dictionary.size(); p++)
   {
-      arq << p << "   " << dictionary[p] << endl;
+    cout << p << "   " << dictionary[p] << endl;
   }
-
 }
 void printFinalTable(vector<string> &dictionary, ofstream &arq)
 {
-  arq << "\nTabela de Simbolos Final" << endl;
-  for(int p=0; p < dictionary.size();p++)
+  cout << "\nTabela de Simbolos Final" << endl;
+  for (int p = 0; p < dictionary.size(); p++)
   {
-      arq << p << "   " << dictionary[p] <<endl;
+    cout << p << "   " << dictionary[p] << endl;
   }
 }
 
 int main()
 {
-  cout << "Aguarde o processamento do arquivo..." << endl;
+  //cout << "Aguarde o processamento do arquivo..." << endl;
   string s;
   string sTotal;
   ifstream in;
   ofstream arq("exit.txt");
 
   in.open("entry.txt");
-  while(!in.eof()) {
-	  getline(in, s);
+  while (!in.eof())
+  {
+    getline(in, s);
     sTotal += s;
   }
   in.close();
@@ -121,8 +122,10 @@ int main()
   vector<int> codified;
   vector<string> dictionary;
 
-  arq << "String lida do arquivo \n" << sTotal << endl;
-  arq << "\nTamanho da string digitada\n" << sTotal.size() << endl;
+  arq << "String lida do arquivo \n"
+      << sTotal << endl;
+  arq << "\nTamanho da string digitada\n"
+      << sTotal.size() << endl;
 
   //Criando o dicionário inicial
   createInitialTable(sTotal, dictionary);
@@ -132,24 +135,20 @@ int main()
   contendo as 256 posições da tabela ASCII, contendo indice e valor.
   O erase irá remover os elementos duplicados.
   */
-  sort(dictionary.begin(),dictionary.end());
-  dictionary.erase( unique( dictionary.begin(), dictionary.end() ), dictionary.end() );
-  
+  sort(dictionary.begin(), dictionary.end());
+  dictionary.erase(unique(dictionary.begin(), dictionary.end()), dictionary.end());
+
   printInitialTable(dictionary, arq);
 
- 
   //Codificação
-  compression(sTotal,codified,dictionary);
+  compression(sTotal, codified, dictionary);
   printFinalTable(dictionary, arq);
   printCodified(codified, arq);
-  
 
-  
   //Decodificação
-  string decodified = decompression(dictionary,codified);
+  string decodified = decompression(dictionary, codified);
   printDecodify(decodified, arq);
 
-
   arq.close();
-  cout << "Arquivo processado, veja o resultado em exit.txt!";
+  //cout << "Arquivo processado, veja o resultado em exit.txt!";
 }
